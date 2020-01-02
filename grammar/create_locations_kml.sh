5f6bff41cee80938108c16e2ce2ca06a444681c2
Steven Ring <smr@southsky.com.au>
2020-01-03 07:26:03 +1100

    Generate merged locations into a single kml file

--- grammar/create_locations_kml.sh
+++ grammar/create_locations_kml.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Create a ``locations.kml`` file by merging all know location definition ``cdl`` files
+
+src_dir=~/projects/locations/docs
 loc_dir=~/planacruise/content/locations
 tmpfile=$(mktemp /tmp/cdl-script.XXXXXX)
+cp $src_dir/*.txt $loc_dir
 echo "vessel Trilogy name: \"Trilogy\" flag: \"Australia\" rego: 12345 speed: 7.5" >> "$tmpfile"
 for filename in $loc_dir/*.txt; do
     cat ${filename} >> "$tmpfile"
