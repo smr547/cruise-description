4ca0005b0270e4977ee170b4666b639b65cb87d6
Steven Ring <smr@southsky.com.au>
2020-01-03 15:57:09 +1100

    Deploy generated web content

--- grammar/deploy_kml.sh
+++ grammar/deploy_kml.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-cruise=$1
-./cdl_preprocessor.py ${cruise}.cdl | ./toKml.py > ${cruise}.kml
-./cdl_preprocessor.py ${cruise}.cdl | ./toMd.py > ${cruise}.md &
-scp ${cruise}.kml smring@thomas.dreamhost.com:planacruise/content/cruises
+# Take the specified cdl file as input, generate all web content and deploy to the web server
+
+
+cdl_file=$1
+season_id=${cdl_file%.cdl}
+echo season id is ${season_id}
+
+./cdl_preprocessor.py ${season_id}.cdl > /tmp/${season_id}.cdl 
+vessel_id=`./vessel_id.py /tmp/${season_id}.cdl`
+echo Vessel is $vessel_id
+
+# create output directory
+tmppath=/tmp/${vessel_id}/${season_id}
+mkdir -p ${tmppath} 
+rm ${tmppath}/*
+
+# generate files
+
+./toKml.py /tmp/${season_id}.cdl > ${tmppath}/chart.kml 
+./toMd.py /tmp/${season_id}.cdl > ${tmppath}/schedule.md 
+./summary_HTML.py /tmp/${season_id}.cdl > ${tmppath}/summary.html 
+./md_html.py ${tmppath}/schedule.md > schedule.html
+./make_source_html.py ${cdl_file} > ${tmppath}/source.html
+cp ${cdl_file} ${tmppath}
+rm /tmp/${season_id}.cdl 
+content_path=/home/planacruise/web_content/
+scp -r /tmp/${vessel_id}  smr@planacruise.online:${content_path}
