f0986bd1b442a38165970f5900e0eb4a10563ab0
Steven Ring <smr@southsky.com.au>
2020-01-05 12:12:11 +1100

    More efficient deployment script

--- grammar/deploy_kml.sh
+++ grammar/deploy_kml.sh
@@ -2,6 +2,8 @@
 
 # Take the specified cdl file as input, generate all web content and deploy to the web server
 
+# exit is any command fails
+set -e
 
 cdl_file=$1
 season_id=${cdl_file%.cdl}
@@ -13,17 +15,21 @@ echo Vessel is $vessel_id
 
 # create output directory
 tmppath=/tmp/${vessel_id}/${season_id}
+if [ -d "${tmppath}" ]; then
+  rm -r ${tmppath}
+fi
 mkdir -p ${tmppath} 
-rm ${tmppath}/*
 
 # generate files
 
 ./toKml.py /tmp/${season_id}.cdl > ${tmppath}/chart.kml 
 ./toMd.py /tmp/${season_id}.cdl > ${tmppath}/schedule.md 
+cp ${tmppath}/schedule.md .
 ./summary_HTML.py /tmp/${season_id}.cdl > ${tmppath}/summary.html 
-./md_html.py ${tmppath}/schedule.md > schedule.html
+./md_html.py ${tmppath}/schedule.md > ${tmppath}/schedule.html
 ./make_source_html.py ${cdl_file} > ${tmppath}/source.html
 cp ${cdl_file} ${tmppath}
 rm /tmp/${season_id}.cdl 
 content_path=/home/planacruise/web_content/
 scp -r /tmp/${vessel_id}  smr@planacruise.online:${content_path}
+rm -r ${tmppath}
