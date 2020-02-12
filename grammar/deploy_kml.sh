6c07f949e402d624fdfcfa05b602186753c2295e
Steven Ring <smr@southsky.com.au>
2020-02-12 18:28:08 +1100

    deploy to test website while developing on openlayers branch

--- grammar/deploy_kml.sh
+++ grammar/deploy_kml.sh
@@ -31,6 +31,6 @@ mkdir -p ${tmppath}
 ./make_source_html.py ${cdl_file} > ${tmppath}/source.html
 cp ${cdl_file} ${tmppath}
 rm /tmp/${season_id}.cdl 
-content_path=/home/planacruise/web_content/
+content_path=/home/planacruise/web_content/test/${vessel_id}/
 scp -r /tmp/${vessel_id}  smr@planacruise.online:${content_path}
 rm -r ${tmppath}
